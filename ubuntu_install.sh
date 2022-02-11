7cd727445f1be2cb5137b6260c341465716efba2
Emilianocm23 <74161942+Emilianocm23@users.noreply.github.com>
2022-02-10 23:45:33 -0600

    Update ubuntu_install.sh

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -10,7 +10,7 @@ echo "==========================================="
 # install depend
 echo "=========== Install Depend ================="
 
-apt install build-essential vim git wget htop curl python3.6 python3.6-dev python3.6-venv python3-pip -y
+apt install build-essential vim git wget htop curl python3 python3-dev python3-venv python3-pip -y
 pip3 install pip --upgrade
 
 
@@ -19,13 +19,8 @@ echo "==========================================="
 # build squid
 
 echo "=========== reBuild Squid ================="
-
 cd /opt
-wget -c http://www.squid-cache.org/Versions/v3/3.5/squid-3.5.27.tar.gz
-tar -zxvf squid-3.5.27.tar.gz
-cd squid-3.5.27
-./configure 'CXXFLAGS=-DMAXTCPLISTENPORTS=65000' --enable-ltdl-convenience
-make && make install
+sudo apt-get install squid3 -y
 chmod 777 /usr/local/squid/var/logs/
 mkdir /var/spool/squid3
 mkdir /etc/squid
@@ -34,14 +29,14 @@ echo "==========================================="
 
 
 echo "=========== Install Python requirements ================="
-cd /opt/
 git clone https://github.com/nguyenanhung/v6proxies.git v6proxies
 cd /opt/v6proxies
-pip3.6 install --upgrade pip setuptools
-pip3.6 install -r requirements.txt
+pip3 install --upgrade pip setuptools
+pip3 install -r requirements.txt
 
 # Increase The Maximum Number Of Open Files
 
 echo "* - nofile 500000" >> /etc/security/limits.conf
 
 echo "=========== Init ================="
+
