e29c0d3ab4050ec0eb012e3dd764941c304e9395
Emilianocm23 <74161942+Emilianocm23@users.noreply.github.com>
2022-02-11 14:43:04 -0600

    Update ipv6.py

--- ipv6.py
+++ ipv6.py
@@ -2,9 +2,9 @@
 import argparse
 import os
 import pathlib
+import netifaces
 from ipaddress import IPv6Network, IPv6Address
 from random import seed, getrandbits, choices, choice
-import netifaces
 from passlib.apache import HtpasswdFile
 
 parser = argparse.ArgumentParser(description='Gen Squid Config')
