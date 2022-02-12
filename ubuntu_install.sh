f22494b813cdb264c4dbb577882b67a1385640ec
Emilianocm23 <74161942+Emilianocm23@users.noreply.github.com>
2022-02-12 11:31:28 -0600

    Update ubuntu_install.sh

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -11,3 +11,8 @@ wget https://raw.githubusercontent.com/Emilianocm23/ipv6/main/requirements.txt;
 pip3 install -r requirements.txt;
 echo "* - nofile 500000" >> /etc/security/limits.conf;
 wget https://raw.githubusercontent.com/Emilianocm23/ipv6/main/ipv6.py;
+cd /opt/
+python3.6 ipv6.py --net_interface enp1s0 --pool_name squidv6 --number_ipv6 500 --unique_ip 1 --start_port 10000 --username lczzgw --password 3gRmJcE2Q
+bash /opt/add_ip_squidv6.sh
+/usr/local/squid/sbin/squid -f /etc/squid/squid-squidv6.conf
+sudo ufw disable
