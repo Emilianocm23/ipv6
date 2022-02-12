9b24128478843de1626981a5a6e588afb7749ff3
Emilianocm23 <74161942+Emilianocm23@users.noreply.github.com>
2022-02-12 14:00:35 -0600

    Update new_test.sh

--- new_test.sh
+++ new_test.sh
@@ -19,7 +19,7 @@ echo "* - nofile 500000" >> /etc/security/limits.conf
 
 echo "=========== Configure IPV6 from python file ================="
 wget https://raw.githubusercontent.com/Emilianocm23/ipv6/main/ipv6.py
-PYTHONPATH=/opt/v6proxies python3.6 ipv6.py --net_interface enp1s0 --pool_name squidv6 --number_ipv6 500 --unique_ip 1 --start_port 10000 --username lczzgw --password 3gRmJcE2Q
+PYTHONPATH=/opt/v6proxies python3 ipv6.py --net_interface enp1s0 --pool_name squidv6 --number_ipv6 500 --unique_ip 1 --start_port 10000 --username lczzgw --password 3gRmJcE2Q
 
 echo "=========== Add all IPS to server and allow connections ================="
 bash /opt/add_ip_squidv6.sh
