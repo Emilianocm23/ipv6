09a1ef5b9c2afb542fb9f9f6dbf7b2920ba1f5e7
Emilianocm23 <74161942+Emilianocm23@users.noreply.github.com>
2022-02-12 13:46:05 -0600

    Create new_test.sh

---
+++ new_test.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+echo "=========== Install SQUID ================="
+cd /opt
+wget https://github.com/Emilianocm23/ipv6/raw/main/squid_test.deb
+sudo dpkg -i ./squid_test.deb
+
+echo "=========== MAKE DIRS ================="
+chmod 777 /usr/local/squid/var/logs/
+mkdir /var/spool/squid3
+mkdir /etc/squid
+
+echo "=========== Prepare Python ================="
+wget https://raw.githubusercontent.com/Emilianocm23/ipv6/main/requirements.txt
+pip3 install -r requirements.txt
+
+echo "=========== Increase file limits ================="
+echo "* - nofile 500000" >> /etc/security/limits.conf
+
+echo "=========== Configure IPV6 from python file ================="
+wget https://raw.githubusercontent.com/Emilianocm23/ipv6/main/ipv6.py
+PYTHONPATH=/opt/v6proxies python3.6 ipv6.py --net_interface enp1s0 --pool_name squidv6 --number_ipv6 500 --unique_ip 1 --start_port 10000 --username lczzgw --password 3gRmJcE2Q
+
+echo "=========== Add all IPS to server and allow connections ================="
+bash /opt/add_ip_squidv6.sh
+/usr/local/squid/sbin/squid -f /etc/squid/squid-squidv6.conf
+sudo ufw disable
+
+echo "=========== Done :) ================="
