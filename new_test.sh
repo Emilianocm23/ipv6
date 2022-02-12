d748cc2b56692687ce7d867a54157d9c3af84b52
Emilianocm23 <74161942+Emilianocm23@users.noreply.github.com>
2022-02-12 14:18:20 -0600

    Update new_test.sh

--- new_test.sh
+++ new_test.sh
@@ -22,6 +22,7 @@ wget https://raw.githubusercontent.com/Emilianocm23/ipv6/main/ipv6.py
 PYTHONPATH=/opt/v6proxies python3 ipv6.py --net_interface enp1s0 --pool_name squidv6 --number_ipv6 500 --unique_ip 1 --start_port 10000 --username lczzgw --password 3gRmJcE2Q
 
 echo "=========== Add all IPS to server and allow connections ================="
+chmod 777 /usr/local/squid/var/logs/cache.log
 bash /opt/add_ip_squidv6.sh
 /usr/local/squid/sbin/squid -f /etc/squid/squid-squidv6.conf
 sudo ufw disable
